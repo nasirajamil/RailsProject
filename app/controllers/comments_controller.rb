5afce29ff7716b05273f26e904972a1407c7e82b
nasirajamil <nasirajamil78@gmail.com>
2021-08-26 19:22:07 +0500

    pushed controller file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :set_article
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+  end
+
+  def index
+    @comments = @article.comments
   end
 
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:article_id])
   end
 end

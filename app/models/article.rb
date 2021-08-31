e22c7b14d5ed625eb349dfa6f8f0ef8551e91afb
nasirajamil <nasirajamil78@gmail.com>
2021-08-31 11:55:52 +0500

    Added error messages to attributes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
+  def a_method_used_for_validation_purposes
+    errors.add(:title, "cannot contain the characters !@#%*()_-+=")
+  end
   has_many :comments, dependent: :destroy
   validates_associated :comments
   validates :title, presence: true,
             length: { minimum: 4 }
+
 
 end

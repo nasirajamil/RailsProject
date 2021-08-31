f17c591ef6e39f9363faa51e1739e95bb455acaa
nasirajamil <nasirajamil78@gmail.com>
2021-08-31 15:25:49 +0500

    used callbacks and callback classes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
+class CallbackForArticle
+  def self.after_destroy(article)
+    puts "my object has destroyed"
+  end
+end
+
 class Article < ApplicationRecord
   def a_method_used_for_validation_purposes
     errors.add(:title, "cannot contain the characters !@#%*()_-+=")
@@ -6,6 +12,15 @@ class Article < ApplicationRecord
   validates_associated :comments
   validates :title, presence: true,
             length: { minimum: 4 }
+  after_validation :capitalize_name
+  after_initialize do |article|
+    puts "You have initialized an object!"
+  end
+  after_destroy CallbackForArticle
 
+  private
+  def capitalize_name
+    self.title = title.capitalize
+  end
 
 end

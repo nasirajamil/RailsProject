46936a639bfe38ffa9cd22d84a647b0dde614f1b
nasirajamil <nasirajamil78@gmail.com>
2021-09-03 16:36:59 +0500

    added queries

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,16 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: "nasira", password: "secret", only: :destroy
 
   before_action :set_article
-  def create
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
-  end
 
   def index
-    @comments = @article.comments
+    #@comments = @article.comments
+    @comments=Comment.find_by_sql("SELECT * FROM comments")
+  end
+
+  def create
+    @comment = @article.comments.create(comment_params)
+    #redirect_to article_path(@article)
+    redirect_to article_path(@article)
   end
 
   def destroy
